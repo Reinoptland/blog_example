e6067d26bb652096dc068ddcb808bee54a467824
reinoptland <reinoptland@gmail.com>
2016-08-09 12:25:09 +0200

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([
-  {name: "Inspectah Deck", email: "dainspectah@wutang.com"},
-  {name: "Masta Killah", email: "mkilla@thewu.com"},
-  {name: "The Genius", email: "voltronhead@wutang.com"},
-  ])
+karl = User.create(name: "Karl Wendlinger", email: "karl@wendlinger.au")
+kamui = User.create(name: "Kamui Kobayashi", email: "kamui@kobayashi.jp")
+jj = User.create(name: "JJ Lehto", email: "jj@lehto.fi")
+andrea = User.create(name: "Andrea de Cesaris", email: "andrea@cesaris.it")
 
 Profile.create([
-  {bio: "He's like the guy who will sit there and watch you lyin' "},
-  {bio: "We have an ABP on an MCkiller; looks like the work of a MASTA"},
-  {bio: "We form like voltron and I happen to be the head"},
+  {bio: "Karl Wendlinger (born 20 December 1968) is an Austrian professional racing and former Formula One driver.", user: karl},
+  {bio: "Kamui Kobayashi is a Japanese professional racing driver who recently competed in Formula One for Caterham. He also previously competed in the GP2 Series, the GP2 Asia Series (which he won), the FIA World Endurance Championship and Formula One from 2009 to 2012, and in 2014.", user: kamui},
+  {bio: "Jyrki Juhani Järvilehto (born 31 January 1966 in Espoo), better known as \"JJ Lehto\", is a racing driver from Finland. He won the 24 Hours of Le Mans twice, in 1995 and 2005. He is also a former Formula One driver.", user: jj},
+  {bio: "Andrea de Cesaris (31 May 1959 – 5 October 2014) was an Italian racing driver. He started 208 Formula One Grands Prix but never won, holding the record for the longest career without a race victory.", user: andrea}
   ])
 
-Post.create([
-  {title: "Chessboxin", post: "The game of chess is like a sword fight
-You must think first before you move
-Toad style is immensely strong and immune to nearly any weapon
-When it's properly used it's almost invincible
-Raw I'ma give it to ya, with no trivia
-Raw like cocaine straight from Bolivia
-My hip-hop will rock and shock the nation
-Like the Emancipation Proclamation
-Weak MC's approach with slang that's dead
-You might as well run into the wall and bang your head
-I'm pushing force, my force you're doubting
-I'm making devils cower to the Caucus Mountains" },
-  {title: "GravelPit", post: "1, 2, 1, 2, yo check this out, it's the jump off right now
-I want everybody, to put your work down, put your guns down
-And report to the pit, the gravel pit
-Leave your problems at home, leave your children at home
-We gon' take it back underground, I be Bobby Boulders
-Wu-Tang Clan on yo' mind one time
-It's the jump off, so just jump off my nigga..."},
-  {title: "Triumph", post: " bomb atomically, Socrates' philosophies
-And hypotheses can't define how I be droppin' these
-Mockeries, lyrically perform armed robbery
-Flee with the lottery, possibly they spotted me
-Battle-scarred shogun, explosion when my pen hits
-Tremendous, ultra-violet shine blind forensics
-I inspect view through the future see millennium
-Killa Beez sold fifty gold sixty platinum
-Shackling the masses with drastic rap tactics
-Graphic displays melt the steel like blacksmiths"},
-  ])
+post_code_in_rails = Post.create(title: "How to code in Rails", post: "Great, great explanation", user: karl)
+post_models =  Post.create(title: "Models", post: "All the great Rails Models", user: karl)
+post_relations =  Post.create(title: "Relations", post: "All the great Relations", user: kamui)
