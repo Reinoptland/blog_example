698da4fc0efe618fc9a6f09a76ed8044f8ba452e
reinoptland <reinoptland@gmail.com>
2016-08-09 12:20:10 +0200

    Added user references to posts and profiles

---
+++ db/migrate/20160809101736_add_user_reference_to.rb
@@ -0,0 +1,4 @@
+class AddUserReferenceTo < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809085551) do
+ActiveRecord::Schema.define(version: 20160809101824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160809085551) do
     t.text     "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 20160809085551) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
